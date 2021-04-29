da54b648edf049e1fc3064d6c3145cb190f50620
Engjell <engjell.ahmeti@uni-bayreuth.de>
2021-04-29 19:28:31 +0200

    -

--- horse_on_the_phonepad.py
+++ horse_on_the_phonepad.py
@@ -26,7 +26,7 @@ class HorseOnPhonepad:
         # self.moves = {0:[4, 6], 1:[6, 8], 2:[7, 9], 3:[4, 8], 4:[0, 3, 9], 5:[], 6:[0, 1, 7], 7:[2, 6], 8:[1, 3], 9:[2, 4]}
         self.moves = self.find_knight_moves()
 
-    # @method findKnightMoves - finds all the moves of a knight in a phone pad.
+    # @method find_knight_moves - finds all the moves of a knight in a phone pad.
     def find_knight_moves(self):
         phonepad = [
             [1, 2, 3],

c2984bc21ccf79e63eae4d32f6ef66f4bedca589
Engjell Ahmeti <engjell.ahmeti@gmail.com>
2022-04-12 21:14:17 +0200

    added the possiblity to run the code with arguments

--- horse_on_the_phonepad.py
+++ horse_on_the_phonepad.py
@@ -18,57 +18,46 @@
         # Your mission is now “Write a software that solves the following question:
         #
         # How many distinct numbers can you dial in N hops from a particular starting position?”
+import sys
 
-class HorseOnPhonepad:
-    # @construrctor HorseOnPhonepad
-    #   - here all the moves for which a knight from either position can make
-    def __init__(self):
-        # self.moves = {0:[4, 6], 1:[6, 8], 2:[7, 9], 3:[4, 8], 4:[0, 3, 9], 5:[], 6:[0, 1, 7], 7:[2, 6], 8:[1, 3], 9:[2, 4]}
-        self.moves = self.find_knight_moves()
+moves = {0:[4, 6], 1:[6, 8], 2:[7, 9], 3:[4, 8], 4:[0, 3, 9], 5:[], 6:[0, 1, 7], 7:[2, 6], 8:[1, 3], 9:[2, 4]}
 
-    # @method find_knight_moves - finds all the moves of a knight in a phone pad.
-    def find_knight_moves(self):
-        phonepad = [
-            [1, 2, 3],
-            [4, 5, 6],
-            [7, 8, 9],
-            [-1, 0, -1],
-        ]
-        moves = [[2, 1], [-2, 1], [2, -1], [-2, -1], [1, 2], [-1, 2], [1, -2], [-1, -2]]
+# @method helper uses recursion to branch on the moves that a knight can jump to
+#       - @param N - number of hops that a knight can do
+#       - @param S - starting position of the night
+def helper(N, S):
+	if N == 0:
+		return 1
 
-        knight_moves = {}
-        for j in range(len(phonepad)):
-            for k in range(len(phonepad[0])):
-                if phonepad[j][k] != -1:
-                    knight_moves[phonepad[j][k]] = []
-                    for move in moves:
-                        position = (j + move[0], k + move[1])
-                        if position[0] >= 0 and position[1] >= 0 and position[0] < len(phonepad) and position[
-                            1] < len(phonepad[0]) and phonepad[position[0]][position[1]] >= 0:
-                            if phonepad[position[0]][position[1]] != -1:
-                                temp_list = knight_moves[phonepad[j][k]]
-                                temp_list.append(phonepad[position[0]][position[1]])
-                                temp_list = set(temp_list)
-                                knight_moves[phonepad[j][k]] = sorted(list(temp_list))
+	result = 0
+	for position in moves[S]:
+		result += helper(N-1, position)
 
-        return knight_moves
+	return result
 
-    # @method helper uses recursion to branch on the moves that a knight can jump to
-    #       - @param N - number of hops that a knight can do
-    #       - @param S - starting position of the night
-    def helper(self, N, S):
-        if N == 0:
-            return 1
+# @method horse_on_phonepad returns the distinct numbers that a knight can dial from a starting position S by doing N hops.
+#       - @param N - number of hops that a knight can do
+#       - @param S - starting position of the night
+def horse_on_phonepad(N, S):
+	return helper(N-1, S)
 
-        result = 0
-        for position in self.moves[S]:
-            result += self.helper(N-1, position)
 
-        return result
+try:
+	args = sys.argv[1:]
+	N, S = -1, -1
 
-    # @method horse_on_phonepad returns the distinct numbers that a knight can dial from a starting position S by doing N hops.
-    #       - @param N - number of hops that a knight can do
-    #       - @param S - starting position of the night
-    def horse_on_phonepad(self, N, S):
-        return self.helper(N-1, S)
+	for index, arg in enumerate(args):
+		if arg == 'N':
+			N = int(args[index+1])
+		elif arg == 'S':
+			S = int(args[index+1])
+	if N == -1 or S == -1:
+		print('The arguments are not correct!!!')
+		print('Example: "python horse_on_the_phonepad.py N 4 S 1"')
+		sys.exit(0)
+		
+	print('You can dial {0} distinct numbers in {1} hops from a starting position of {2}.'.format(horse_on_phonepad(N, S), N, S))
 
+except Exception as e:
+	print('The arguments are not correct!!!')
+	print('Example: "python horse_on_the_phonepad.py N 4 S 1"')
